1d3940466ab60ffdbde96ce03f83b3f9c8da1029
Danila <danillegend1996@gmail.com>
2025-04-03 21:55:15 +1000

    ready

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  target: 'static',
+  generate: {
+    fallback: true
+  },
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxt/ui'],
   pinia: {
