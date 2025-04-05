208118ad7c7225606a7b5ea09e09c7f810738a3f
Danila <danillegend1996@gmail.com>
2025-04-05 20:11:36 +1000

    Updates

---
+++ _nuxt/CXncCSls.js
@@ -0,0 +1,1 @@
+import{u as t}from"./Bg95tJt3.js";import{N as e,L as o}from"./DIneU_vK.js";const n=e(async(a,r)=>{if(!t().isAuthenticated)return o("/login")});export{n as default};
---
+++ _nuxt/D27Ac5ye.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,a as n,o as r}from"./DIneU_vK.js";const s={};function a(c,e){return r(),t("div",null,e[0]||(e[0]=[n("h1",null,"Печенька",-1)]))}const _=o(s,[["render",a]]);export{_ as default};
